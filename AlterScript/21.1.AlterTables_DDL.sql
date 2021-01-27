fe492f5bb1bb5a1bd0f25504c2b35ee97d26f167
garumuga <58757921+garumuga@users.noreply.github.com>
2021-01-27 07:44:17 +0530

    Update 21.1.AlterTables_DDL.sql

--- AlterScript/21.1.AlterTables_DDL.sql
+++ AlterScript/21.1.AlterTables_DDL.sql
@@ -5,7 +5,6 @@ ALTER TABLE dbo.nova_DBAUTOMATION_POC4 ADD  DemoLocation varchar(255);
 ALTER TABLE nova_DBAUTOMATION_POC5 DROP COLUMN DemoDate,DemoName;
 
 
-
 --Create Table script for new tables
 
 CREATE TABLE nova_DBAUTOMATION_POC11 (
